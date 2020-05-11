f1b3d85a4efe24c9c1e75899892b630f10552559
sawyer1997 <rishabhverma220898@gmail.com>
2020-05-12 01:41:55 +0530

    handled case when page is not found

--- core/views.py
+++ core/views.py
@@ -21,16 +21,22 @@ redis_client = Redis()
 def fetch_videos(request, page):
 	if request.method == 'GET':
 		try:
+			logger.info('Page requested: {}'.format(page))
 			redis_value = redis_client.get(REDIS_KEY)
 			if redis_value:
 				stored_content = eval(redis_value.decode('utf-8'))
 			else:
 				stored_content = youtube_data_api()
-				redis_client.set(REDIS_KEY, str(stored_content), ex=300)
+				redis_client.set(REDIS_KEY, str(stored_content), ex=70)
 			p = Paginator(stored_content, CONTENTS_IN_ONE_PAGE)
+			content = {}
 			if page > 0 and page <= p.num_pages:
 				content = p.page(page).object_list
 				logger.info('Content: {}'.format(content))
 				return Response(content, status=status.HTTP_200_OK)
+			else:
+				logger.error('Page not found, total pages: {}, page requested: {}'.format(p.num_pages, page))
+				return Response({'message': 'Page not found'}, status=status.HTTP_404_NOT_FOUND)
 		except Exception as e:
 			logger.error('Something went wrong while fetching video content!!')
+			return Response({}, status=status.HTTP_404_NOT_FOUND)
