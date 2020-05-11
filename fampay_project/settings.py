3d68727d192efee9f99653116d4b13caef086935
sawyer1997 <rishabhverma220898@gmail.com>
2020-05-12 01:42:48 +0530

    logging settings were handled

--- fampay_project/settings.py
+++ fampay_project/settings.py
@@ -138,3 +138,17 @@ CACHES = {
         }
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
